4b38dee974848cc135a3c485071b7b8f0a894e47
duyquang25 <buiduyquang25@gmail.com>
2021-08-11 21:14:43 +0700

    dom & event

---
+++ 20210804_JS-E/JS-E.M.A1101/main.js
@@ -0,0 +1,47 @@
+const images = ['images/img1.jpg', 'images/img2.png', 'images/img3.png'];
+let index = 0;
+ 
+// Add your code here
+// Task 1
+
+imgContainer = document.querySelector(".img-container");
+// console.log(imgContainer);
+// elem = imgContainer.createElement("img");
+// elem1 = document.querySelector(".img-container").appendChild(elem);
+// console.log(elem1);
+backBtn = document.querySelector(".back");
+nextBtn = document.querySelector(".next");
+
+backBtn.addEventListener('click', function() {
+  // Add your code here
+  // Task 3.1
+  index--;
+  if(index < 0 ){
+    index = 0 ;
+  }
+  renderImage();
+
+});
+
+nextBtn.addEventListener('click', function() {
+  // Add your code here
+  // Task 3.2
+
+  index++;
+  if (index >= images.length) {
+    index = 0;
+  }
+  renderImage();
+});
+
+// Add your code here
+// Task 2
+
+
+
+function renderImage(){
+  imgContainer.innerHTML = `<img src="${images[index]}">`;
+}
+
+renderImage()
+  
